77c605c5993cc3ff7625e559c63985c70606d58c
Keylan King <kingkey0101@outlook.com>
2025-06-03 23:43:04 -0500

    adding footer androutes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import Nav from './components/Nav'
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted'
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Books from "./Pages/Books";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path='/' exact component={Home} />
+        <Route path='/books' exact component={Books} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
- 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,13 @@ import LibraryLogo from '../assets/Library.svg'
 
 
 const Nav = () => {
+  function openMenu() {
+    document.body.classList += ' menu--open'
+  }
+
+  function closeMenu(){
+    document.body.classList.remove('menu--open')
+  }
   return (
     <nav>
       <div className="nav__container">
@@ -17,11 +24,11 @@ const Nav = () => {
             </a>
           </li>
           <li className="nav__list">
-            <a href="/" className="nav__link">
+            <a href="/" className="nav__link nav__link--books">
                 Books
             </a>
           </li>
-          <button className="btn__menu">
+          <button className="btn__menu" onClick={openMenu}>
             <FontAwesomeIcon icon='bars' />
           </button>
           <li className="nav__icon">
@@ -32,7 +39,7 @@ const Nav = () => {
           </li>
         </ul>
         <div className="menu__backdrop">
-          <button className="btn__menu btn__menu--close">
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
             <FontAwesomeIcon icon='times' />
           </button>
           <ul className="menu__links">
