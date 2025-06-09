399b8319af563ea63c1894d2c27dceb427f260c9
Keylan King <kingkey0101@outlook.com>
2025-06-09 14:59:45 -0500

    adding recommended 5 star books under selected books

--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -3,10 +3,11 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import Rating from "../components/UI/Rating";
 import Price from "../components/UI/Price";
+import Book from "../components/UI/Book";
 
 const BookInfo = ({ books }) => {
   const { id } = useParams();
-  const book = books.find(book => +book.id === +id);
+  const book = books.find((book) => +book.id === +id);
 
   return (
     <div id="books__body">
@@ -23,19 +24,16 @@ const BookInfo = ({ books }) => {
             </div>
             <div className="book__selcted">
               <figure className="book__selected--figure">
-                <img
-                  src={book.url}
-                  alt=""
-                  className="book__selected--img"
-                />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                  {book.title}
-                </h2>
+                <h2 className="book__selected--title">{book.title}</h2>
                 <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                  <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
                 </div>
                 <div className="book__summary">
                   <h3 className="book__summary--title">Summary</h3>
@@ -61,6 +59,14 @@ const BookInfo = ({ books }) => {
           <div className="row">
             <div className="book__selected--top">
               <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
             </div>
           </div>
         </div>
