94748deec5f1113d0a4123d587b8cd50931ab98b
Keylan King <kingkey0101@outlook.com>
2025-06-08 23:15:45 -0500

    adding Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import Books from "./Pages/Books";
 import {books} from './data'
-
+import BookInfo from "./Pages/BookInfo";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Nav />
         <Route path='/' exact component={Home} />
         <Route path='/books' exact render={ () => <Books books={books} /> } />
+        <Route path='/books/1' render={() => <BookInfo books={books} />} /> 
         <Footer />
       </div>
     </Router>
